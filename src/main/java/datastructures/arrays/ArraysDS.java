bae6e88efec0e9864c472956388bb222c7fd4c37
ragnarzone <ragnarzone@gmail.com>
2022-10-07 14:38:31 +0300

    added Arrays - DS

---
+++ src/main/java/datastructures/arrays/ArraysDS.java
@@ -0,0 +1,13 @@
+package datastructures.arrays;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ArraysDS {
+
+    // Solution
+    public static List<Integer> reverseArray(List<Integer> a) {
+        Collections.reverse(a);
+        return a;
+    }
+}
