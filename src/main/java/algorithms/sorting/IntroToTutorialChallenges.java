3ebf1d09f3b044b7450a5e9ff8e6c3a7a39562ce
ragnarzone <ragnarzone@gmail.com>
2022-10-07 15:58:46 +0300

    Added Intro to Tutorial Challenges for sorting solution

---
+++ src/main/java/algorithms/sorting/IntroToTutorialChallenges.java
@@ -0,0 +1,10 @@
+package algorithms.sorting;
+
+import java.util.List;
+
+public class IntroToTutorialChallenges {
+    //Solution
+    public static int introTutorial(int V, List<Integer> arr) {
+        return arr.indexOf(V);
+    }
+}
