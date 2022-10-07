b40545b07b08aa5ab59616de6db953d331505b4e
ragnarzone <ragnarzone@gmail.com>
2022-10-07 14:20:06 +0300

    added Beautiful Triplets solution

--- src/main/java/BeautifulTriplets.java
+++ src/main/java/BeautifulTriplets.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BeautifulTriplets {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeautifulTriplets {
+    int d = 0;
+    List<Integer> arr = new ArrayList<>();
+
+    // Solution
+    public static int beautifulTriplets(int d, List<Integer> arr) {
+
+        int counter = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.contains(arr.get(i)+d) && arr.contains(arr.get(i)+2*d)){
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
