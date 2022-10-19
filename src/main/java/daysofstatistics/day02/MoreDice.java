09340178cf70f4482c3552ab50b5ca7741614768
ragnarzone <ragnarzone@gmail.com>
2022-10-19 14:44:30 +0300

    added solution for MoreDice of 10 Days of Statistics

---
+++ src/main/java/daysofstatistics/day02/MoreDice.java
@@ -0,0 +1,7 @@
+package daysofstatistics.day02;
+
+public class MoreDice {
+    // sample space: 6 * 6 = 36;
+    // favourable outcomes: (1,5), (2,4), (4,2), (5,1);
+    // P(sum = 6 && d1 != d2) = 4/36 = 1/9;
+}
