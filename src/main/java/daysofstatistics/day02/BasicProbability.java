f3f6035a3e994df7614755050c81a84856d1b7ca
ragnarzone <ragnarzone@gmail.com>
2022-10-19 14:30:24 +0300

    added solution for BasicProbability of 10 Days of Statistics

---
+++ src/main/java/daysofstatistics/day02/BasicProbability.java
@@ -0,0 +1,9 @@
+package daysofstatistics.day02;
+
+public class BasicProbability {
+    // favourable outcomes: when sum <= 9;
+    // sample space = 6 * 6 = 36;
+    // complement of favourable outcomes: when sum > 9;
+    // calculate complement: (4,6), (5,5), (5,6), (6,4), (6,5), (6,6)
+    // probability(sum <= 9) = 1 - 6/36 = 5/6;
+}
