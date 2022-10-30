284b9c2f8bd08fcd085ff1bdc2e0108220b7e7d4
ragnarzone <ragnarzone@gmail.com>
2022-10-30 09:41:42 +0200

    added solution to Inheritance of 30DaysOfCode

---
+++ src/main/java/daysofcode/day12/Inheritance.java
@@ -0,0 +1,68 @@
+package daysofcode.day12;
+
+import java.util.Scanner;
+
+public class Inheritance {
+    static class Person {
+        protected String firstName;
+        protected String lastName;
+        protected int idNumber;
+
+        // Constructor
+        Person(String firstName, String lastName, int identification){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.idNumber = identification;
+        }
+
+        // Print person data
+        public void printPerson(){
+            System.out.println(
+                    "Name: " + lastName + ", " + firstName
+                            + 	"\nID: " + idNumber);
+        }
+
+    }
+
+    static class Student extends Person{
+        private int[] testScores;
+
+        public Student(String firstName, String lastName, int id, int[] testScores){
+            super(firstName, lastName, id);
+            this.testScores = testScores;
+        }
+
+        public char calculate(){
+            int average = 0;
+            for (int i : testScores) {
+                average += i;
+            }
+            average /= (double) testScores.length;
+            if(average < 40) return 'T';
+            else if (average >=40 && average < 55) return 'D';
+            else if (average >= 55 && average < 70) return 'P';
+            else if (average >=70 && average < 80) return 'A';
+            else if (average >= 80 && average < 90) return 'E';
+            else return 'O';
+        }
+    }
+
+    class Solution {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String firstName = scan.next();
+            String lastName = scan.next();
+            int id = scan.nextInt();
+            int numScores = scan.nextInt();
+            int[] testScores = new int[numScores];
+            for(int i = 0; i < numScores; i++){
+                testScores[i] = scan.nextInt();
+            }
+            scan.close();
+
+            Student s = new Student(firstName, lastName, id, testScores);
+            s.printPerson();
+            System.out.println("Grade: " + s.calculate() );
+        }
+    }
+}
