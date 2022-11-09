210c32cfed7209bb66a6cb8dbb9b2c4816f34e99
ragnarzone <ragnarzone@gmail.com>
2022-11-09 14:07:19 +0200

    added solution to BSTLevelOrderTraversal of 30DaysOfCode

---
+++ src/main/java/daysofcode/day23/BSTLevelOrderTraversal.java
@@ -0,0 +1,66 @@
+package daysofcode.day23;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BSTLevelOrderTraversal {
+    static class Node{
+        Node left,right;
+        int data;
+        Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+    }
+    static class Solution{
+
+        static void levelOrder(Node root){
+            ArrayDeque<Node> deque = new ArrayDeque<>();
+            if(root == null){
+                return;
+            } else {
+                deque.add(root);
+                while(!deque.isEmpty()){
+                    Node n = deque.removeFirst();
+                    System.out.print(n.data + " ");
+                    if(n.left != null){
+                        deque.add(n.left);
+                    }
+                    if(n.right != null){
+                        deque.add(n.right);
+                    }
+                }
+            }
+
+
+        }
+
+        public static Node insert(Node root,int data){
+            if(root==null){
+                return new Node(data);
+            }
+            else{
+                Node cur;
+                if(data<=root.data){
+                    cur=insert(root.left,data);
+                    root.left=cur;
+                }
+                else{
+                    cur=insert(root.right,data);
+                    root.right=cur;
+                }
+                return root;
+            }
+        }
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }
+    }
+}
