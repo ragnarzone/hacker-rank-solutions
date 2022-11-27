af00e382228be6955a0819bab22e716d55e41034
ragnarzone <ragnarzone@gmail.com>
2022-11-27 21:11:23 +0200

    added solution to InsertingNodeIntoASortedDoublyLinkedList

---
+++ src/main/java/datastructures/linkedlists/InsertingNodeIntoASortedDoublyLinkedList.java
@@ -0,0 +1,125 @@
+package datastructures.linkedlists;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InsertingNodeIntoASortedDoublyLinkedList {
+    public static class Solution {
+
+        static class DoublyLinkedListNode {
+            public int data;
+            public DoublyLinkedListNode next;
+            public DoublyLinkedListNode prev;
+
+            public DoublyLinkedListNode(int nodeData) {
+                this.data = nodeData;
+                this.next = null;
+                this.prev = null;
+            }
+        }
+
+        static class DoublyLinkedList {
+            public DoublyLinkedListNode head;
+            public DoublyLinkedListNode tail;
+
+            public DoublyLinkedList() {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void insertNode(int nodeData) {
+                DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+                if (this.head == null) {
+                    this.head = node;
+                } else {
+                    this.tail.next = node;
+                    node.prev = this.tail;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+            while (node != null) {
+                bufferedWriter.write(String.valueOf(node.data));
+
+                node = node.next;
+
+                if (node != null) {
+                    bufferedWriter.write(sep);
+                }
+            }
+        }
+
+        public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data) {
+            DoublyLinkedListNode toInsert = new DoublyLinkedListNode(data);
+            if(llist == null){
+                return toInsert;
+            } else if(data < llist.data){
+                toInsert.next = llist;
+                llist.prev = toInsert;
+                return toInsert;
+            } else {
+                DoublyLinkedListNode temp = llist;
+                while(temp.data < data){
+                    if(temp.next != null){
+                        temp = temp.next;
+                    } else {
+                        if(temp.data < data){
+                            toInsert.prev = temp;
+                            temp.next = toInsert;
+                            return llist;
+                        }
+                    }
+                }
+
+                temp.prev.next = toInsert;
+                toInsert.prev = temp.prev;
+                toInsert.next = temp;
+                temp.prev = toInsert;
+                return llist;
+            }
+        }
+
+
+
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) throws IOException {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int t = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int tItr = 0; tItr < t; tItr++) {
+                DoublyLinkedList llist = new DoublyLinkedList();
+
+                int llistCount = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                for (int i = 0; i < llistCount; i++) {
+                    int llistItem = scanner.nextInt();
+                    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                    llist.insertNode(llistItem);
+                }
+
+                int data = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                DoublyLinkedListNode llist1 = sortedInsert(llist.head, data);
+
+                printDoublyLinkedList(llist1, " ", bufferedWriter);
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.close();
+
+            scanner.close();
+        }
+    }
+}
